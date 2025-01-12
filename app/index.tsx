9844922cba1b3a848c039851791c3ffbe9fefd52
ogarmela <52357639+ogarmela@users.noreply.github.com>
2025-01-11 22:51:40 -0800

    request pages (#33)
    * request pages
    
    * Create request.tsx page
    
    Created request.tsx page
    
    ---------
    
    Co-authored-by: NW Miller <nwmiller.516@gmail.com>
    Co-authored-by: Nicholas Wade Miller <36825393+nwm516@users.noreply.github.com>

--- app/index.tsx
+++ app/index.tsx
@@ -5,10 +5,28 @@ import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
-import { supabase } from '@/supabaseClient';
-import { Link } from 'expo-router';
+// import { supabase } from '@/supabaseClient';
 
 export default function HomeScreen() {
+  const [message, setMessage] = useState('Fetching...');
+
+  // const fetchMessage = async () => {
+  //   const { data, error } = await supabase
+  //     .from('messages')
+  //     .select('text')
+  //     .eq('id', 1) 
+  //     .single();
+  //   if (error) {
+  //     console.error(error);
+  //     setMessage('Error fetching message');
+  //   } else {
+  //     setMessage(data.text);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   fetchMessage();
+  // }, []);
 
   return (
     <ParallaxScrollView
@@ -25,6 +43,7 @@ export default function HomeScreen() {
         <HelloWave />
         <Link href="/help">Help Page</Link>
       </ThemedView>
+
     </ParallaxScrollView>
   );
 }
