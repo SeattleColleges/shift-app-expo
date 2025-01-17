74deb8b6ffba769a1140b2c19eb9674dcd7f60dc
cshimm <cameronjshimmin@gmail.com>
2025-01-16 18:05:06 -0800

    updated co-workers.tsx to display CoworkersList.tsx

--- app/co-workers.tsx
+++ app/co-workers.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import { Image, StyleSheet, Platform, View, Text, Button } from 'react-native';
-
+import { StyleSheet } from 'react-native';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import CoworkersList from "@/components/CoworkersList";
 
 export default function CoWorkersPage() {
   return (
-      <ThemedView style={[styles.stepContainer, styles.container]}>
-        <ThemedText type="title">Coworkers</ThemedText>
+      <ThemedView style={[styles.container]}>
+        <ThemedText
+            style={styles.title}
+            type="title"
+        >
+          Coworkers
+        </ThemedText>
         <CoworkersList />
       </ThemedView>
   );
@@ -20,11 +24,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'flex-start',
   },
-
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-    paddingHorizontal: 16,
-    paddingTop: 16,
+  title: {
+    marginVertical: 8,
   },
 });
