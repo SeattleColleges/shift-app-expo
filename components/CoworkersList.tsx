789bf532ab1c364fce5a8424e6819f67164f5e29
cshimm <cameronjshimmin@gmail.com>
2025-01-16 18:04:33 -0800

    added CoworkerList.tsx

---
+++ components/CoworkersList.tsx
@@ -0,0 +1,39 @@
+import {FlatList, ListRenderItem, SafeAreaView, StyleSheet, View} from "react-native";
+import {coworkersDummyData} from "@/data/dummyCoworkerData";
+import {CoworkerListItem, CoworkerProps} from "@/components/CoworkerListItem";
+import React from "react";
+
+export default function CoworkersList() {
+    const renderCoworker: ListRenderItem<CoworkerProps> = ({ item }) =>
+        <CoworkerListItem
+            id={item.id}
+            name={item.name}
+            role={item.role}
+            profileImageUrl={item.profileImageUrl}
+
+        />;
+    interface ItemSeparatorProps {
+        height?: number;  // Optional prop to control the spacing height
+    }
+
+    const ItemSeparator: React.FC<ItemSeparatorProps> = ({ height = 10 }) => {
+        return <View style={{ height }} />;
+    };
+    return (
+        <View style={styles.coworkersList} >
+            <FlatList
+                data={coworkersDummyData}
+                renderItem={renderCoworker}
+                ItemSeparatorComponent={() => <ItemSeparator height={30}/>}
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    coworkersList: {
+        flex: 1,
+        width: '100%',
+        paddingBottom: 20
+    }
+});
