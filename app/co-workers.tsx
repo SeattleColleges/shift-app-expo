cf2e8a5b62c73b4eacfa151ce015b787ede3bf98
cshimm <cameronjshimmin@gmail.com>
2025-01-16 17:12:38 -0800

    added 'role' type to ThemedText

--- app/co-workers.tsx
+++ app/co-workers.tsx
@@ -1,35 +1,24 @@
 import React from 'react';
 import { Image, StyleSheet, Platform, View, Text, Button } from 'react-native';
 
-import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import CoworkersList from "@/components/CoworkersList";
 
 export default function CoWorkersPage() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Co-workers Page Placeholder</ThemedText>
-        <Text style={styles.message}>
-          This is a placeholder for the Co-workers page.
-        </Text>
+      <ThemedView style={[styles.stepContainer, styles.container]}>
+        <ThemedText type="title">Coworkers</ThemedText>
+        <CoworkersList />
       </ThemedView>
-    </ParallaxScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
+  container: {
+    flex: 1,
     alignItems: 'center',
-    gap: 8,
+    justifyContent: 'flex-start',
   },
 
   stepContainer: {
@@ -38,20 +27,4 @@ const styles = StyleSheet.create({
     paddingHorizontal: 16,
     paddingTop: 16,
   },
-
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-
-  message: {
-    fontSize: 24,
-    marginBottom: 20,
-    color: '#333',
-    textAlign: 'center',
-  },
-  
 });
