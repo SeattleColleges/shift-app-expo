5935eff257e93f13b95e96be799caccdc443a90d
Marco Bejarano Oseguera <marco.bejarano03@gmail.com>
2025-01-20 23:32:23 -0800

    added the required fields, and now I am working on getting it styled a bit

--- components/UserDetails.tsx
+++ components/UserDetails.tsx
@@ -1,16 +1,39 @@
 import React from 'react'
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export const UserDetails = () => {
+  const firstName = "first name";
+  const lastName = "last name";
+  const dateHired = "2222-22-22";
+  const dept = "department";
+  const supervisor = "supervisor";
+  const email = "email";
+  const phoneNumber = "(206) 999-9999";
   return (
     <>
-        <View>
+        <View style={styles.container}>
             <View>
                 <View>
                     <Text>User Deets</Text>
+                    <Text>First Name: {firstName}</Text>
+                    <Text>Last Name: {lastName}</Text>
+                    <Text>Date Hired: {dateHired}</Text>
+                    <Text>Department: {dept}</Text>
+                    <Text>Supervisor: {supervisor}</Text>
+                    <Text>Email: {email}</Text>
+                    <Text>Phone Number: {phoneNumber}</Text>
                 </View>
             </View>
         </View>
     </>
   )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+
+})
