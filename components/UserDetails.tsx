e70da5ed103b020dc47a2094ce5cc8957d402cbb
Marco Bejarano Oseguera <marco.bejarano03@gmail.com>
2025-01-20 22:49:15 -0800

    working on user details component

---
+++ components/UserDetails.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Text, View } from "react-native";
+
+export const UserDetails = () => {
+  return (
+    <>
+        <View>
+            <View>
+                <View>
+                    <Text>User Deets</Text>
+                </View>
+            </View>
+        </View>
+    </>
+  )
+}
