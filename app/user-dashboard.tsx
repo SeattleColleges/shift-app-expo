fc9a8aedf08e8f936cb44fe13da37fbfca7dd29a
cshimm <cameronjshimmin@gmail.com>
2025-01-28 19:08:01 -0800

    replace old code with option toggle component

--- app/user-dashboard.tsx
+++ app/user-dashboard.tsx
@@ -3,19 +3,9 @@ import { Image, StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { supabase } from '@/supabaseClient';
+import {OptionToggle} from "@/components/dashboard/OptionToggle";
 
 export default function UserDashboard() {
-  const [activeButtonTopRow, setActiveButtonTopRow] = useState('Pending');
-  const [activeButtonBottomRow, setActiveButtonBottomRow] = useState('Week');
-
-  const handlePressTopRow = (button: string) => {
-    setActiveButtonTopRow(button);
-  };
-
-  const handlePressBottomRow = (button: string) => {
-    setActiveButtonBottomRow(button);
-  }
-
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
@@ -29,85 +19,8 @@ export default function UserDashboard() {
       {/* Schedule Title*/}
       <View style={styles.scheduleContainer}>
         <Text style={styles.scheduleTitle}>Schedule</Text>
-
-        {/* Top Row of Buttons */}
-
-        {/* Pending button */}
-        <View style={styles.buttonRow}>
-          <TouchableOpacity
-            style={[
-              styles.button,
-              activeButtonTopRow === 'Pending' && styles.buttonActive,
-            ]}
-            onPress={() => handlePressTopRow('Pending')}
-          >
-            <Text style={[
-              styles.buttonText,
-              activeButtonTopRow === 'Pending' && styles.buttonTextActive,
-            ]}
-            >
-              Pending
-            </Text>
-          </TouchableOpacity>
-
-          {/* Approved button */}
-          <TouchableOpacity
-            style={[
-              styles.button,
-              activeButtonTopRow === 'Approved' && styles.buttonActive,
-            ]}
-            onPress={() => handlePressTopRow('Approved')}
-          >
-            <Text
-              style={[
-                styles.buttonText,
-                activeButtonTopRow === 'Approved' && styles.buttonTextActive,
-              ]}
-            >
-              Approved
-            </Text>
-          </TouchableOpacity>
-        </View>
-
-        {/* Bottom Row of Buttons */}
-
-        {/* Week button */}
-        <View style={styles.buttonRow}>
-          <TouchableOpacity
-            style={[
-              styles.button,
-              activeButtonBottomRow === 'Week' && styles.buttonActive,
-            ]}
-            onPress={() => handlePressBottomRow('Week')}
-          >
-            <Text
-              style={[
-                styles.buttonText,
-                activeButtonBottomRow === 'Week' && styles.buttonTextActive,
-              ]}
-            >
-              Week
-            </Text>
-          </TouchableOpacity>
-
-          {/* Month button */}
-          <TouchableOpacity
-            style={[
-              styles.button,
-              activeButtonBottomRow === 'Month' && styles.buttonActive,
-            ]}
-            onPress={() => handlePressBottomRow('Month')}
-          >
-            <Text
-              style={[
-                styles.buttonText,
-                activeButtonBottomRow === 'Month' && styles.buttonTextActive,
-              ]}
-            >
-              Month
-            </Text>
-          </TouchableOpacity>
-        </View>
+        <OptionToggle option1={'Week'} option2={'Month'}/>
+        <OptionToggle option1={'Pending'} option2={'Approved'}/>
       </View>
 
       {/* Placeholder Box */}
@@ -136,31 +49,6 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 16,
-  },
-  buttonRow: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginBottom: 14,
-    width: '100%',
-  },
-  button: {
-    flex: 1,
-    backgroundColor: '#CFD8DC',
-    paddingVertical: 12,
-    marginHorizontal: 8,
-    borderRadius: 8,
-    alignItems: 'center',
-  },
-  buttonActive: {
-    backgroundColor: 'black',
-  },
-  buttonText: {
-    color: 'black',
-    fontSize: 12,
-  },
-  buttonTextActive: {
-    color: 'white',
-    fontSize: 12,
   },
   placeholderBox: {
     marginTop: 16,
