6f466d9bb41a38a5d51c4bd1a8540dc1a1b8b214
Isaac J <jimenez.isaac.jr@gmail.com>
2025-01-30 16:33:35 -0800

    Add temporary pages

---
+++ app/(tabs)/add-shift.tsx
@@ -0,0 +1,9 @@
+import {View,Text} from "react-native";
+
+export default function addShift() {
+    return (
+        <View>
+            <Text>Add shifts placeholder pager</Text>
+        </View>
+    )
+}
