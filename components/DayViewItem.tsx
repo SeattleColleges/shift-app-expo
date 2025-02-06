2c0dcf3c416acaa7156e50db998d5620149e7818
cshimm <cameronjshimmin@gmail.com>
2025-02-05 18:42:36 -0800

    update DayViewItem.tsx

--- components/DayViewItem.tsx
+++ components/DayViewItem.tsx
@@ -1,32 +1,36 @@
 import {Text, View} from "react-native";
 import React from "react";
-import {ShiftData} from "@/data/dummyShiftData";
 
-interface DayViewItemProps {
-    id: number;
-    shift: ShiftData;
-}
-
-interface Props{
+interface Props {
     item: any,
 }
 
 export const DayViewItem = (props: Props) => {
     const {item} = props;
-    const numHoursScheduled = Number(item.endTime.split(':')[0]) - Number(item.startTime.split(':')[0]);
+    const start = Number(item.startTime.split(':')[0]);
+    const end = Number(item.endTime.split(':')[0]);
+    const numHoursScheduled = end - start;
     return (
-        <View style={{backgroundColor:'white'}}>
-            <View style={{marginBottom: 10, width: '70%', margin: 15, backgroundColor: '#CFD8DC', padding: 10, paddingLeft: 20, borderRadius: 20}}>
-                <Text>{item.role}</Text>
-                <View style={{flexDirection: 'row', justifyContent: 'flex-start', gap: 5}}>
-                    <Text>{item.building}:</Text>
+        <View style={{backgroundColor: 'white'}}>
+            <View style={{
+                marginBottom: 10,
+                width: '70%',
+                margin: 15,
+                backgroundColor: '#CFD8DC',
+                padding: 10,
+                paddingLeft: 20,
+                borderRadius: 20
+            }}>
+                <Text style={{fontSize: 16}}>{item.role}</Text>
+                <Text style={{flexDirection: 'row', justifyContent: 'flex-start', fontSize: 12, color: '#3e3e3e'}}>
+                    <Text>{item.building}: </Text>
                     <Text>{item.roomNumber}</Text>
-                </View>
-                <View style={{flexDirection: 'row', justifyContent: 'flex-start'}}>
+                </Text>
+                <Text style={{flexDirection: 'row', justifyContent: 'flex-start', fontSize: 16, marginVertical: 4}}>
                     <Text>{numHoursScheduled} HR | </Text>
-                    <Text>{item.startTime} - {item.endTime}</Text>
+                    <Text>{`${item.startTime} ${start >= 12 ? "PM" : "AM"} - ${item.endTime} ${end >= 12 ? "PM" : "AM"}`}</Text>
                     <Text>{item.duration}</Text>
-                </View>
+                </Text>
             </View>
         </View>
     )
