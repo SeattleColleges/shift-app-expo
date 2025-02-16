5445c73d6639bd4c334e1dcf34ee1d6fdf194239
cshimm <cameronjshimmin@gmail.com>
2025-02-16 10:40:26 -0800

    added shift detail placeholder

---
+++ app/shift-details-page/[id].tsx
@@ -0,0 +1,9 @@
+import {SafeAreaView, Text} from "react-native";
+
+export default function ShiftDetailsPage () {
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+            <Text>Test</Text>
+        </SafeAreaView>
+    )
+}
