a224ff722b56ffbb32ae90fae06f6b075ac49676
cshimm <cameronjshimmin@gmail.com>
2025-02-16 11:02:49 -0800

    display basic shift info

--- app/shift-details-page/[id].tsx
+++ app/shift-details-page/[id].tsx
@@ -1,9 +1,17 @@
 import {SafeAreaView, Text} from "react-native";
+import {useLocalSearchParams} from "expo-router";
 
 export default function ShiftDetailsPage () {
+    const item = useLocalSearchParams()
+    console.log("item", item)
     return (
         <SafeAreaView style={{ flex: 1 }}>
-            <Text>Test</Text>
+            <Text>{item.title}</Text>
+            <Text>{item.role}</Text>
+            <Text>{item.building}: {item.roomNumber}</Text>
+            <Text>{item.date}</Text>
+            <Text>{item.startTime} - {item.endTime}</Text>
         </SafeAreaView>
     )
 }
+
