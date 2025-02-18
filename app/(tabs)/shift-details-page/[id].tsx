c72fa41e370c11f9fbbfea6d438eb2c1364f3e24
cshimm <cameronjshimmin@gmail.com>
2025-02-17 18:02:23 -0800

    styling

--- app/(tabs)/shift-details-page/[id].tsx
+++ app/(tabs)/shift-details-page/[id].tsx
@@ -1,21 +1,50 @@
-import {StyleSheet, Text, View} from "react-native";
+import {Pressable, StyleSheet, Text, View} from "react-native";
 import {useLocalSearchParams} from "expo-router";
 import {ThemedText} from "@/components/ThemedText";
 import {ThemedView} from "@/components/ThemedView";
+import {weekdays, months} from "moment";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function ShiftDetailsPage () {
-    const item = useLocalSearchParams()
+    const item = useLocalSearchParams();
+    const date = new Date(item.date as string)
+    const day = date.getDate();
+    const month = months()[date.getMonth()];
+    const dayOfWeek = weekdays()[date.getDay()];
+    const formattedDate = `${dayOfWeek}, ${month} ${day}`
+    type PressableIconProps = {
+        name: keyof typeof Ionicons.glyphMap;
+        size?: number;
+        onPress:() => void
+    };
+    const PressableIcon = ({name, size = 20, onPress}: PressableIconProps) => {
+        return (
+            <Pressable onPress={onPress}>
+                <Ionicons
+                    size={size}
+                    name={name}
+                />
+            </Pressable>
+        )
+    }
+
     return (
         <ThemedView style={styles.container}>
+            <View style={styles.dateHeader}>
+                <PressableIcon name={'arrow-back'} onPress={() => console.log('prev')} />
+                <ThemedText type={'default'}>{formattedDate}</ThemedText>
+                <PressableIcon name={'arrow-forward'} onPress={() => console.log('next')} />
+            </View>
             <View style={styles.detailsContainer}>
-                <ThemedText style={{marginBottom: 10, alignSelf: 'center'}} type={'subtitle'}>
+                <ThemedText style={{alignSelf: 'center'}} type={'subtitle'}>
                     Shift Details
                 </ThemedText>
                 <Text>Hours Scheduled: {item.numHoursScheduled}</Text>
                 <Text>Time: {item.startTime} - {item.endTime}</Text>
                 <Text>Role: {item.role}</Text>
-                <Text>Building: {item.building}: {item.roomNumber}</Text>
-                <Text>{item.date}</Text>
+                <Text>Building: {item.building} - {item.roomNumber}</Text>
+                <Text>Supervisor: </Text>
+                <Text>Coworkers: </Text>
             </View>
         </ThemedView>
     )
@@ -25,23 +54,22 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: "center",
-        justifyContent: "center",
     },
     detailsContainer: {
-        justifyContent: "flex-start",
         alignItems: "flex-start",
-        borderRadius: 20,
-        height: 300,
+        top: 50,
+        borderRadius: 10,
         width: 300,
         backgroundColor: "#eee",
-        padding: 10,
+        padding: 15,
+        paddingBottom: 20,
         gap: 20
     },
-    heading: {
-        textAlign: "center",
-        paddingBottom: 20,
-    },
-    text: {
-        paddingBottom: 10,
-    },
+    dateHeader: {
+        alignItems:'center',
+        height: 80,
+        width: "75%",
+        flexDirection:'row',
+        justifyContent:'space-around'
+    }
 })
