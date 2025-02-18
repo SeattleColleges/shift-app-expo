ac56dd1ea8a24ec9b63aef10c7d22ffba9834e0c
cshimm <cameronjshimmin@gmail.com>
2025-02-17 18:54:06 -0800

    added shift request button, additional styling

--- app/(tabs)/shift-details-page/[id].tsx
+++ app/(tabs)/shift-details-page/[id].tsx
@@ -1,12 +1,14 @@
-import {Pressable, StyleSheet, Text, View} from "react-native";
+import {Pressable, StyleSheet, Text, useColorScheme, View} from "react-native";
 import {useLocalSearchParams} from "expo-router";
 import {ThemedText} from "@/components/ThemedText";
 import {ThemedView} from "@/components/ThemedView";
 import {weekdays, months} from "moment";
 import Ionicons from "@expo/vector-icons/Ionicons";
+import {Colors} from '@/constants/Colors'
 
 export default function ShiftDetailsPage () {
     const item = useLocalSearchParams();
+    const colorScheme = useColorScheme() || 'light';
     const date = new Date(item.date as string) || new Date()
     const day = date.getDate();
     const month = months()[date.getMonth()];
@@ -17,17 +19,30 @@ export default function ShiftDetailsPage () {
         size?: number;
         onPress:() => void
     };
+    type ShiftRequestButtonProps = {
+        text: string;
+        onPress: () => void;
+    }
     const PressableIcon = ({name, size = 20, onPress}: PressableIconProps) => {
         return (
             <Pressable onPress={onPress}>
                 <Ionicons
                     size={size}
                     name={name}
+                    color={Colors[colorScheme].text}
                 />
             </Pressable>
         )
     }
-
+    const ShiftRequestButton = ({text, onPress}: ShiftRequestButtonProps) => {
+        return (
+            <Pressable onPress={onPress} style={[styles.button, {backgroundColor: Colors[colorScheme].text,}]}>
+                <Text style={{color: colorScheme == 'light' ? Colors.dark.text: Colors.light.text}}>
+                    {text}
+                </Text>
+            </Pressable>
+        )
+    }
     return (
         <ThemedView style={styles.container}>
             <View style={styles.dateHeader}>
@@ -35,17 +50,21 @@ export default function ShiftDetailsPage () {
                 <ThemedText type={'default'}>{formattedDate}</ThemedText>
                 <PressableIcon name={'arrow-forward'} onPress={() => console.log('next')} />
             </View>
-            <View style={styles.detailsContainer}>
-                <ThemedText style={{alignSelf: 'center'}} type={'subtitle'}>
+            <ThemedView style={styles.detailsContainer}>
+                <Text style={{alignSelf: 'center', fontWeight:'500', fontSize: 18}}>
                     Shift Details
-                </ThemedText>
+                </Text>
                 <Text>Hours Scheduled: {item.numHoursScheduled}</Text>
                 <Text>Time: {item.startTime} - {item.endTime}</Text>
                 <Text>Role: {item.role}</Text>
                 <Text>Building: {item.building} - {item.roomNumber}</Text>
                 <Text>Supervisor: </Text>
                 <Text>Coworkers: </Text>
-            </View>
+            </ThemedView>
+            <ThemedView style={{flexDirection: 'row', gap: 25}}>
+                <ShiftRequestButton onPress={() => console.log('give up shift')} text={'GIVE UP SHIFT'}/>
+                <ShiftRequestButton onPress={() => console.log('take shift')} text={'TAKE SHIFT'}/>
+            </ThemedView>
         </ThemedView>
     )
 }
@@ -54,10 +73,10 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: "center",
+        gap: 50
     },
     detailsContainer: {
         alignItems: "flex-start",
-        top: 50,
         borderRadius: 10,
         width: 300,
         backgroundColor: "#eee",
@@ -71,5 +90,9 @@ const styles = StyleSheet.create({
         width: "75%",
         flexDirection:'row',
         justifyContent:'space-around'
+    },
+    button: {
+        padding: 12,
+        borderRadius: 5
     }
 })
