4760b6cfa7df5745572d2b6c78a085070b64b7a6
cshimm <cameronjshimmin@gmail.com>
2025-03-11 23:15:53 -0700

    add User type

---
+++ app/types/User.ts
@@ -0,0 +1,12 @@
+export type User = {
+    firstName: string,
+    middleName: string,
+    lastName: string,
+    dateHired: string,
+    dept: string,
+    supervisor: string,
+    email: string,
+    phone: string,
+    pronouns: string,
+    role: string,
+}
