a5b51d186642127bff1f4fbdcca7c059c00a1a6f
Isaac J <jimenez.isaac.jr@gmail.com>
2025-03-23 14:05:47 -0700

    Implement boolean logic with supervisor deny function

--- supabase/snippets/6_shift_changes_mgmt.sql
+++ supabase/snippets/6_shift_changes_mgmt.sql
@@ -21,6 +21,7 @@ CREATE TABLE shift_changes
     og_shift_profile_id       INT REFERENCES profiles (profile_int_id),
     covering_profile_id       INT REFERENCES profiles (profile_int_id),
     approved_by_supervisor_id INT REFERENCES supervisors (supervisor_id),
+    denied_by_supervisor_id   INT REFERENCES supervisors (supervisor_id),
     status                    SHIFT_STATUS NOT NULL DEFAULT 'unclaimed',
     coverage_reason           TEXT,
     requested_at              TIMESTAMPTZ           DEFAULT NOW()
@@ -31,7 +32,7 @@ CREATE
     shift_id_param INT,
     coverage_reason_param TEXT DEFAULT 'No reason provided'
 )
-    RETURNS VOID AS
+    RETURNS BOOLEAN AS
 $$
 DECLARE
     shift_rec RECORD;
@@ -56,6 +57,7 @@ BEGIN
     INSERT INTO shift_changes(shift_id, og_shift_profile_id, coverage_reason)
     VALUES (shift_rec.shift_id, shift_rec.assigned_user_id, coverage_reason_param);
 
+    RETURN TRUE;
 -- Handle errors, returns to func caller
 EXCEPTION
     WHEN OTHERS THEN
@@ -69,13 +71,13 @@ CREATE
     OR REPLACE FUNCTION shift_owner_removed_shift(
     shift_id_param INT
 )
-    RETURNS VOID AS
+    RETURNS BOOLEAN AS
 $$
 DECLARE
     shift_rec RECORD;
 BEGIN
     -- Update, change status to removed, and set shift's needs_coverage to false
-    SELECT *
+    SELECT status
     INTO shift_rec
     FROM shift_changes
     WHERE shift_id = shift_id_param;
@@ -100,6 +102,7 @@ BEGIN
     ELSE
         RAISE EXCEPTION 'Shift w/ ID % is approved, cannot be removed', shift_id_param;
     END IF;
+    RETURN TRUE;
 END;
 $$
     LANGUAGE plpgsql;
@@ -110,13 +113,14 @@ CREATE
     shift_id_param INT,
     covering_profile_id_param INT
 )
-    RETURNS VOID AS
+    RETURNS BOOLEAN AS
 $$
 BEGIN -- Change shift status to pending when shift claimed
     UPDATE shift_changes
     SET covering_profile_id = covering_profile_id_param,
         status              = 'pending'
     WHERE shift_id = shift_id_param;
+    RETURN TRUE;
 END;
 $$
     LANGUAGE plpgsql;
@@ -126,42 +130,91 @@ CREATE OR REPLACE FUNCTION update_shift_w_profile_ids(
     shift_id_param INT,
     supervisor_id_param INT
 )
-    RETURNS VOID AS
+    RETURNS BOOLEAN AS
 $$
 DECLARE
     shift_change_rec RECORD;
 BEGIN -- Check supervisor privileges, update shift, update needs_coverage to false
-    SELECT covering_profile_id,shift_change_id
+    SELECT covering_profile_id, shift_change_id
     INTO shift_change_rec
     FROM shift_changes
     WHERE shift_id = shift_id_param;
 
--- Handle missing record
+-- Handle missing record data
     IF
         NOT FOUND THEN
         RAISE EXCEPTION 'Shift w/ ID % not found', shift_id_param;
-    END IF;
-
-    IF
-        EXISTS(SELECT 1
-               FROM supervisors
-               WHERE supervisor_id = supervisor_id_param)
-    THEN
-        UPDATE shift_changes
-        SET approved_by_supervisor_id = supervisor_id_param
-        WHERE shift_id = shift_id_param;
-
-        UPDATE shifts
-        SET assigned_user_id = shift_change_rec.covering_profile_id,
-            needs_coverage   = FALSE
-        WHERE shift_id = shift_id_param;
     ELSE
-        RAISE EXCEPTION 'Error updating shift_change w/ id %',
-            shift_change_rec.shift_change_id;
+        IF shift_change_rec.covering_profile_id IS NULL THEN
+            RAISE EXCEPTION 'Shift w/ ID % missing covering profile id', shift_id_param;
+        END IF;
+        IF
+            EXISTS(SELECT 1
+                   FROM supervisors
+                   WHERE supervisor_id = supervisor_id_param)
+        THEN
+            UPDATE shift_changes
+            SET approved_by_supervisor_id = supervisor_id_param
+            WHERE shift_id = shift_id_param;
+
+            UPDATE shifts
+            SET assigned_user_id = shift_change_rec.covering_profile_id,
+                needs_coverage   = FALSE
+            WHERE shift_id = shift_id_param;
+            RETURN TRUE;
+        ELSE
+            RAISE EXCEPTION 'Error updating shift change in shift_change_id %',
+                shift_change_rec.shift_change_id;
+        END IF;
     END IF;
 END;
+$$ LANGUAGE plpgsql;
+
+-- Deny requested shift change
+CREATE OR REPLACE FUNCTION deny_shift_change(
+    shift_id_param INT,
+    supervisor_id_param INT
+)
+    RETURNS BOOLEAN AS
 $$
-    LANGUAGE plpgsql;
+BEGIN
+    DECLARE
+        shift_change_rec RECORD;
+    BEGIN -- Check any missing data, then supervisor privileges, save deny supervisor id and update needs coverage
+        SELECT covering_profile_id, shift_change_id
+        INTO shift_change_rec
+        FROM shift_changes
+        WHERE shift_id = shift_id_param;
+
+-- Handle missing records
+        IF
+            NOT FOUND THEN
+            RAISE EXCEPTION 'Shift w/ ID % not found', shift_id_param;
+        ELSE
+            IF shift_change_rec.covering_profile_id IS NULL THEN
+                RAISE EXCEPTION 'Shift w/ ID % missing covering profile id', shift_id_param;
+            END IF;
+            IF
+                EXISTS(SELECT 1
+                       FROM supervisors
+                       WHERE supervisor_id = supervisor_id_param)
+            THEN
+                UPDATE shift_changes
+                SET denied_by_supervisor_id = supervisor_id_param
+                WHERE shift_id = shift_id_param;
+
+                UPDATE shifts
+                SET needs_coverage = FALSE
+                WHERE shift_id = shift_id_param;
+                RETURN TRUE;
+            ELSE
+                RAISE EXCEPTION 'Error updating shift change in shift_change_id %',
+                    shift_change_rec.shift_change_id;
+            END IF;
+        END IF;
+    END;
+END;
+$$ LANGUAGE plpgsql;
 
 /*
 Testing: simulate user pressing give up or removing shift
@@ -175,8 +228,8 @@ SELECT add_shift_to_shift_change(999);
 SELECT shift_owner_removed_shift(1);  -- Changes status to removed
 SELECT shift_owner_removed_shift(999);
 
-select add_covering_id_to_shift_change(2,3);
-select update_shift_w_profile_ids(2,2); -- Validates ids in previous query, 3 fails, 5 passes
+SELECT add_covering_id_to_shift_change(2,3);
+SELECT update_shift_w_profile_ids(2,2); -- Validates ids in previous query, 3 fails, 5 passes
 
 -- Query
 SELECT *, convert_pacific_tz(requested_at) AS pacific_time FROM shift_changes;
