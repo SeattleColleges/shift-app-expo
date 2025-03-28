2fb7222319040d3fb5e6f436f319d5b3ed3386e8
Isaac J <jimenez.isaac.jr@gmail.com>
2025-03-27 22:20:45 -0700

    Add frontend calls based on db tests in shift_change_mgmt file

---
+++ app/db-function-calls.tsx
@@ -0,0 +1,158 @@
+import {
+    View,
+    Text,
+    useColorScheme,
+    StyleSheet,
+    Pressable,
+    TextInput,
+    ScrollView
+} from "react-native";
+import React, {useEffect, useState} from "react";
+import {createClient} from '@supabase/supabase-js';
+
+// Create admin client to connect w/ server
+const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL!
+const SERVICE_ROLE_KEY = process.env.EXPO_PUBLIC_SUPABASE_SERVICE_ROLE_KEY!
+const supabaseAdmin = createClient(SUPABASE_URL, SERVICE_ROLE_KEY);
+
+const paramObj = {
+    add_shift_to_shift_change: {1: 'shift_id_param', 2: 'coverage_reason_param'},
+    shift_owner_removed_shift: {1: 'shift_id_param'},
+    add_covering_id_to_shift_change: {1: 'shift_id_param', 2: 'covering_profile_id_param'},
+    approve_update_shift_w_profile_ids: {1: 'shift_id_param', 2: 'supervisor_id_param'},
+    deny_shift_change: {1: 'shift_id_param', 2: 'supervisor_id_param'},
+    get_shift_data: {1: 'shift_id_param'}
+}
+
+
+const ToggleOption = ({name, setToggleValue}) => {
+    return (
+        <View>
+            <Pressable
+                style={styles.button}
+                onPress={() => {
+                    console.log(name)
+                    setToggleValue(name)
+                }}>
+                <Text style={{margin: 5}}>{name}</Text>
+            </Pressable>
+        </View>
+    )
+}
+
+export default function DBFunctionCalls() {
+    const colorScheme = useColorScheme();
+    const [result, setResult] = useState(null);
+    const [idA, setIdA] = useState('');
+    const [idB, setIdB] = useState('');
+    const [toggleValue, setToggleValue] = useState('');
+
+    //console.log(JSON.stringify(supabaseAdmin,null,2))
+    // Test a query
+    const get_data = async (funcName, paramsObj) => {
+        const {data, error} = await supabaseAdmin.rpc(funcName, {...paramsObj});
+
+        if (error) {
+            setResult(error)
+        } else {
+            setResult(data)
+        }
+    }
+
+    return (
+        <ScrollView>
+
+            <View style={styles.titleContainer}>
+
+                <Text>Database Testing</Text>
+                <View style={{flexDirection: 'row'}}>
+                    <Text style={{marginHorizontal: 10}}>id_param_1</Text>
+                    <Text style={{marginHorizontal: 10}}>id_param_1</Text>
+                </View>
+                <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+                    <TextInput
+                        style={styles.input}
+                        onChangeText={setIdA}
+                        value={idA}
+                        keyboardType="numeric"
+                    />
+                    <TextInput
+                        style={styles.input}
+                        onChangeText={setIdB}
+                        value={idB}
+                        keyboardType="numeric"
+                    />
+                </View>
+
+                <View style={{flexDirection: 'column', justifyContent: 'space-between', gap: 2}}>
+                    <ToggleOption name={'add_shift_to_shift_change'} setToggleValue={setToggleValue}/>
+                    <ToggleOption name={'shift_owner_removed_shift'} setToggleValue={setToggleValue}/>
+                    <ToggleOption name={'add_covering_id_to_shift_change'} setToggleValue={setToggleValue}/>
+                    <ToggleOption name={'approve_update_shift_w_profile_ids'} setToggleValue={setToggleValue}/>
+                    <ToggleOption name={'deny_shift_change'} setToggleValue={setToggleValue}/>
+                    <ToggleOption name={'get_shift_data'} setToggleValue={setToggleValue}/>
+                </View>
+
+                <Pressable
+                    style={styles.button}
+                    onPress={() => {
+                        console.log('Call ' + toggleValue + ' pressed');
+                        let inputParams = {}
+                        const params = paramObj[toggleValue]
+
+                        if (Object.keys(params).length === 1) {
+                            inputParams[params[1]] = idA
+                        } else {
+                            inputParams[params[1]] = idA
+                            inputParams[params[2]] = idB
+                        }
+                        get_data(toggleValue, inputParams)
+                    }}>
+                    <Text style={{margin: 10}}> {toggleValue ? 'Call ' + toggleValue : 'Press functions above'}</Text>
+                </Pressable>
+
+                <View style={styles.resultContainer}>
+                    <Text style={styles.resultText}>
+                        {result ? JSON.stringify(result, null, 2) : 'No result'}
+                    </Text>
+                </View>
+            </View>
+        </ScrollView>
+    );
+}
+
+const styles = StyleSheet.create({
+    titleContainer: {
+        flexDirection: "column",
+        alignItems: "center",
+        gap: 5,
+        width: "100%",
+        height: "100%",
+        backgroundColor: "skyblue",
+        color: "#000",
+        paddingTop: "10%",
+        paddingHorizontal: 20
+    },
+    button: {
+        borderStyle: "solid",
+        borderWidth: 1,
+        backgroundColor: "#2196F3",
+        paddingHorizontal: 10
+    },
+    resultContainer: {
+        width: "100%",
+        padding: 10,
+        backgroundColor: "#fff",
+        borderRadius: 5
+    },
+    resultText: {
+        fontFamily: "monospace"
+    },
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+        backgroundColor: '#fff'
+    },
+});
