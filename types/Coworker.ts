b0c3c450e806efeb356ae1d53a429317e57be733
cshimm <cameronjshimmin@gmail.com>
2025-05-01 22:43:16 -0700

    Add Coworker type

---
+++ types/Coworker.ts
@@ -0,0 +1,8 @@
+export type Coworker = {
+    id: number,
+    name: string,
+    role?: string | undefined
+    profileImageUrl?: string | undefined | null,
+    positionId?: string | undefined,
+    departmentId?: string | undefined,
+}
