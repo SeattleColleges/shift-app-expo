75eb90f1b01810392a7f6fb1be5c62dd4badcb92
Miguel Quintana <97195623+MiguelQuintana1@users.noreply.github.com>
2025-05-10 11:57:45 -0700

    creation of the component EmployeeTimePage, thinking in a future data integration, with the capasity of being tested by user role

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
-import React, { ReactElement } from "react";
+import React from "react";
+import type { ReactElement } from "react";
 import { Image, StyleSheet } from "react-native";
 import { HelloWave } from "@/components/HelloWave";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
@@ -57,6 +58,9 @@ export default function HomeScreen(): ReactElement {
         </Link>
         <Link href={"/db-function-calls"} style={linkStyle}>
           DB Function Calls
+        </Link>
+        <Link href={"/employee-time-page"} style={linkStyle}>
+          Employee Time Page
         </Link>
       </ThemedView>
     </ParallaxScrollView>
