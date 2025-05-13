1f74134c6ad96132d421aab6bebe686cf6aecc33
Isaac J <jimenez.isaac.jr@gmail.com>
2025-05-12 20:57:07 -0700

    Rework date regex

--- data/utils.ts
+++ data/utils.ts
@@ -1,15 +1,5 @@
 export const dateTimeFormatter= (date:Date) => {
-    return new Intl.DateTimeFormat("en-US", {
-        timeZone: "America/Los_Angeles",
-        year: 'numeric',
-        month: '2-digit',
-        day: '2-digit',
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit',
-        fractionalSecondDigits: 3,
-        hour12: true
-    }).format(date);
+    return new Date(date.toLocaleString('en-US', { timeZone: 'America/Los_Angeles' }));
 }
 
 export const extractUTC = (str:string) => {
