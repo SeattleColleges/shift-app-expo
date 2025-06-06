277991733287f2315de9cb9f0bc8668d937c37b0
Isaac J <jimenez.isaac.jr@gmail.com>
2025-06-05 20:15:57 -0700

    Rename func

--- app/(tabs)/calendar/_layout.tsx
+++ app/(tabs)/calendar/_layout.tsx
@@ -1,6 +1,6 @@
 import { Stack } from 'expo-router';
 
-export default function ScheduleStack() {
+export default function ScheduleLayout() {
     return (
         <Stack>
             <Stack.Screen
